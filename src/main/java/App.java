b0103bd707176363f510c6610429f683a6d423c4
stanley10603 <stanley10603@gmail.com>
2018-05-16 11:49:27 +0000

    20180516GGG

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world.GGGGGG";
     }
 
     public static void main(String[] args) {
